6f4f9f1246aaf43be1dfadb6570ca18ee971d21a
Jason Lim <limjas@oregonstate.edu>
2018-04-02 15:10:01 -0700

    updated ant class

--- Ant.cpp
+++ Ant.cpp
@@ -1,5 +1,15 @@
-//
-// Created by jaslim on 4/2/2018.
-//
+/*********************************************************************
+** Program Filename: Ant.hpp
+        ** Author: Jason Lim
+** Date: April 15th, 2018
+** Description: This is the implementation file for Langston's Ant class
+*********************************************************************/
 
 #include "Ant.hpp"
+
+~Ant::Ant(); //destructor to delete Ant
+void Ant::setRow(int row); //row position of ant
+void Ant::setCol(int col); //column position of ant
+void Ant::gameBoard(); //to create a board and initialize it
+void Ant::antStart(); //starting position of ant on board
+void Ant::moveAnt(); //using algorithm to move the ant
--- Ant.hpp
+++ Ant.hpp
@@ -1,6 +1,9 @@
-//
-// Created by jaslim on 4/2/2018.
-//
+/*********************************************************************
+** Program Filename: Ant.hpp
+** Author: Jason Lim
+** Date: April 15th, 2018
+** Description: This is the header file for Langston's Ant class
+*********************************************************************/
 
 #ifndef ANT_HPP
 #define ANT_HPP
@@ -8,10 +11,23 @@
 #include "Board.hpp"
 #include <iostream>
 
+enum Orientation {North, West, East, South};
+
 class Ant {
 
 private:
-    int rowAnt, colAnt, step;
+    int step, rowAnt, colAnt;
+    Orientation antsOrientation = North;
+    Board *boardFunction;
+
+public:
+
+    ~Ant(); //destructor to delete Ant
+    void setRow(int row); //row position of ant
+    void setCol(int col); //column position of ant
+    void gameBoard(); //to create a board and initialize it
+    void antStart(); //starting position of ant on board
+    void moveAnt(); //using algorithm to move the ant
 
 
 };
