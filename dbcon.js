2776f3e3c64a2b6e8c349a2785623f11131900dc
Jason Lim <limjas@os1.engr.oregonstate.edu>
2019-05-05 15:53:13 -0700

    dbcon template

--- dbcon.js
+++ dbcon.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs340_limjas',
-  password        : '2425',
+  password        : 'xxxx',
   database        : 'cs340_limjas'
 });
 
