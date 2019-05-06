7ed756f6a9508e7d964d5795af1010219cb135d9
Jason Lim <limjas@flip1.engr.oregonstate.edu>
2019-05-05 20:45:12 -0700

    jason 4th commit

--- diagnostic.js
+++ diagnostic.js
@@ -1,15 +1,54 @@
 var express = require('express');
-var mysql = require('./dbcon.js');
+var mysql = require('mysql');
 
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var pool = mysql.createPool({
+	host: 'classmysql.engr.oregonstate.edu',
+	user: 'cs340_limjas',
+	password: '2425',
+	database: 'cs340_limjas'
+});
+
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 14250);
+app.set('port', 44250);
 
 app.get('/',function(req,res) {
 	res.render('home');
+});
+
+var selectData = function(res, table) {
+  var control = {};
+  pool.query('SELECT * FROM ' + table, function(err, rows, fields) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    control.results = rows;
+    res.send(control);
+  });
+};
+
+app.get('/customers', function(req, res) {
+	selectData(res, 'customerstable');      
+});
+
+app.get('/products', function(req, res) {
+	selectData(res, 'productstable');
+});
+
+app.get('/workers', function(req, res) {
+	selectData(res, 'workers');
+});
+
+app.get('/sections', function(req, res) {
+	selectData(res, 'sections');
+});
+
+app.get('/locations', function(req, res) {
+	selectData(res, 'locations');
 });
 
 app.use(function(req,res){
