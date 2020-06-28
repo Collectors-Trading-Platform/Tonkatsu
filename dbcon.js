59ca1e60d4bf32fb79e4845a096833a2a70b3de0
Jason Lim <31980053+jhhlim@users.noreply.github.com>
2020-06-28 14:22:44 -0700

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_limjas',
-  password        : '2425',
-  database        : 'cs340_limjas'
+  host            : 'ec2-52-207-25-133.compute-1.amazonaws.com',
+  user            : 'tfoczylyskmhtl',
+  password        : '8ed88d7b9e016b0995155a3cfbc71114bfb8ea8f02d75f1953188564cf18728a',
+  database        : 'd41n2g43kclvmr'
 });
 
 module.exports.pool = pool;
