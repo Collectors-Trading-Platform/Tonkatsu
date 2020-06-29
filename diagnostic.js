de0e03d3605cc66891890c091cb0a30a27db74eb
Jason Lim <31980053+jhhlim@users.noreply.github.com>
2020-06-28 23:17:41 -0700

    Update diagnostic.js

--- diagnostic.js
+++ diagnostic.js
@@ -140,5 +140,5 @@ app.use(function(err, req, res, next){
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-    console.log("Listening on " + port);
+    console.log(port);
 });
