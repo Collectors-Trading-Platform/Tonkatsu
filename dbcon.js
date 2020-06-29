fa10e384d4052818005190fa79895e1c5f9eb1cf
tomki1 <38170380+tomki1@users.noreply.github.com>
2020-06-28 22:13:35 -0700

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -1,10 +1,10 @@
 var mysql = require('pg');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'ec2-52-207-25-133.compute-1.amazonaws.com',
-  user            : 'tfoczylyskmhtl',
-  password        : '8ed88d7b9e016b0995155a3cfbc71114bfb8ea8f02d75f1953188564cf18728a',
-  database        : 'd41n2g43kclvmr'
+  host            : 'us-cdbr-east-02.cleardb.com',
+  user            : 'b5740ca304b5bc',
+  password        : 'fee9d940',
+  database        : 'heroku_f8290df26fb9a01'
 });
 
 module.exports.pool = pool;
