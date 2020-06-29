9924e64939c11b1905fd50565fab299d8377d194
Jason Lim <31980053+jhhlim@users.noreply.github.com>
2020-06-28 23:03:18 -0700

    Update diagnostic.js

--- diagnostic.js
+++ diagnostic.js
@@ -7,7 +7,7 @@ var pool = mysql.createPool({
 	host: 'us-cdbr-east-02.cleardb.com',
 	user: 'b5740ca304b5bc',
 	password: 'fee9d940',
-	database: 'heroku_f8290df26fb9a01'
+	database: 'f8290df26fb9a01'
 });
 
 
