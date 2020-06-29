b17ea959ebc12aa9c37e18bf5ec181bfc9befb81
tomki1 <38170380+tomki1@users.noreply.github.com>
2020-06-28 23:10:04 -0700

    Update diagnostic.js

--- diagnostic.js
+++ diagnostic.js
@@ -1,22 +1,44 @@
 var express = require('express');
 var mysql = require('mysql');
 var app = express();
-var bodyParser = require('body-parser');
-var handlebars = require('express-handlebars').create({defaultLayout:'main'});
-var pool = mysql.createPool({
+app.use(express.logger());
+
+
+
+var db_config = {
 	host: 'us-cdbr-east-02.cleardb.com',
 	user: 'b5740ca304b5bc',
 	password: 'fee9d940',
-	database: 'f8290df26fb9a01'
-});
+	database: 'heroku_f8290df26fb9a01'
+};
+
+var connection;
+
+function handleDisconnect() {
+    console.log('1. connecting to db:');
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+													// the old one cannot be reused.
+
+    connection.connect(function(err) {              	// The server is either down
+        if (err) {                                     // or restarting (takes a while sometimes).
+            console.log('2. error when connecting to db:', err);
+            setTimeout(handleDisconnect, 1000); // We introduce a delay before attempting to reconnect,
+        }                                     	// to avoid a hot loop, and to allow our node script to
+    });                                     	// process asynchronous requests in the meantime.
+    											// If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+        console.log('3. db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { 	// Connection to the MySQL server is usually
+            handleDisconnect();                      	// lost due to either server restart, or a
+        } else {                                      	// connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+
+handleDisconnect();
 
 
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
-app.set('port', 3306));
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(bodyParser.json());
-app.use('/', express.static('public'));
 
 app.get('/reset-table',function(req,res,next){
   var context = {};
@@ -116,8 +138,7 @@ app.use(function(err, req, res, next){
   res.render('500');
 });
 
-app.listen(app.get('port'), function(){
-  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+    console.log("Listening on " + port);
 });
-return router
-}();
