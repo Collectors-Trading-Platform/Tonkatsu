ee657120332b4d12605f8861a3813ff3b67b5edf
Jason Lim <31980053+jhhlim@users.noreply.github.com>
2020-06-29 21:33:05 -0700

    Update diagnostic.js

--- diagnostic.js
+++ diagnostic.js
@@ -1,42 +1,28 @@
 var express = require('express');
 var mysql = require('./dbcon.js');
 var app = express();
-app.use(express.logger());
 
+var session  = require('express-session');
+var cookieParser = require('cookie-parser');
+var morgan = require('morgan');
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+var mysql = require('./dbcon.js');
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-var connection = mysql.createConnection({
-	host: 'us-cdbr-east-02.cleardb.com',
-	user: 'b5740ca304b5bc',
-	password: 'fee9d940',
-	database: 'heroku_f8290df26fb9a01'
-});
+app.use(bodyParser.json());
 
+app.use(session({
+	secret: process.env.session_secret,
+	resave: true,
+	saveUninitialized: true
+ } )); // session secret
 
-function handleDisconnect() {
-    console.log('1. connecting to db:');
-    connection = mysql.createConnection(db_config); // Recreate the connection, since
-													// the old one cannot be reused.
-
-    connection.connect(function(err) {              	// The server is either down
-        if (err) {                                     // or restarting (takes a while sometimes).
-            console.log('2. error when connecting to db:', err);
-            setTimeout(handleDisconnect, 1000); // We introduce a delay before attempting to reconnect,
-        }                                     	// to avoid a hot loop, and to allow our node script to
-    });                                     	// process asynchronous requests in the meantime.
-    											// If you're also serving http, display a 503 error.
-    connection.on('error', function(err) {
-        console.log('3. db error', err);
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') { 	// Connection to the MySQL server is usually
-            handleDisconnect();                      	// lost due to either server restart, or a
-        } else {                                      	// connnection idle timeout (the wait_timeout
-            throw err;                                  // server variable configures this)
-        }
-    });
-}
-
-handleDisconnect();
-
-
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 app.get('/reset-table',function(req,res,next){
   var context = {};
