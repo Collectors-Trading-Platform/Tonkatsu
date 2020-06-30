13538ce48bb0f323e68bf03e62f1391028f33d78
Jason Lim <31980053+jhhlim@users.noreply.github.com>
2020-06-29 22:21:39 -0700

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use('/static', express.static('public'));
 app.set('view engine', 'handlebars');
 
-app.set('port', 20555);
+app.set('port', process.env.PORT || 5000);
 app.set('mysql', mysql);
 app.use('/workers', require('./workers.js'));
 app.use('/products', require('./products.js'));
