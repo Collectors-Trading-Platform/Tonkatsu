adb8e471bbc4947e8938b74979dd0f4bd51bc643
tomki1 <38170380+tomki1@users.noreply.github.com>
2020-06-30 18:38:42 -0700

    Update diagnostic.js

--- diagnostic.js
+++ diagnostic.js
@@ -6,10 +6,16 @@ var app = express();
 // app.use(express.logger());
 
 var db_config = {
+	/**
   	host            : 'us-cdbr-east-02.cleardb.com',
  	user            : 'b5740ca304b5bc',
  	password        : 'fee9d940',
  	database        : 'heroku_f8290df26fb9a01'
+	**/
+	host            : 'oniddb.cws.oregonstate.edu',
+	user            : 'tomki-db',
+	password        : 'tomki-db',
+ 	database        : 'Dq3vSeRCSEd7zPrW'
 };
 
 var connection;
