2177e2d394b88519370a504ddccdb54960511405
tomki1 <38170380+tomki1@users.noreply.github.com>
2020-06-30 17:01:09 -0700

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'oniddb.cws.oregonstate.edu',
-  user            : 'tomki-db',
-  password        : 'tomki-db',
-  database        : 'Dq3vSeRCSEd7zPrW'
+  host            : 'us-cdbr-east-02.cleardb.com',
+  user            : 'b5b7cb5d8126ac',
+  password        : 'b296bab6',
+  database        : 'heroku_fd3a3a2208266f1'
 });
 //port 3306
 
