e94bd909bf0aafe2cf1ae618c3b52273e62d697a
tomki1 <38170380+tomki1@users.noreply.github.com>
2020-06-30 23:50:17 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var mysql = require('./dbcon.js');
-var bodyParser = require('body-parser');
+//var express = require('express');
+//var mysql = require('./dbcon.js');
+//var bodyParser = require('body-parser');
 
-var app = express();
-var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+//var app = express();
+//var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -12,15 +12,15 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use('/static', express.static('public'));
 
 
-app.set('port', process.env.PORT || 5000);
-app.set('mysql', mysql);
+//app.set('port', process.env.PORT || 5000);
+//app.set('mysql', mysql);
 //app.use('/workers', require('./workers.js'));
 //app.use('/products', require('./products.js'));
-app.use('/customers', require('./customers.js'));
+//app.use('/customers', require('./customers.js'));
 //app.use('/locations', require('./locations.js'));
 //app.use('/sections', require('./sections.js'));
 //app.use('/customersproducts', require('./customersproducts.js'));
-app.use('/', express.static('public'));
+//app.use('/', express.static('public'));
 
 app.get('/',function(req,res) {
 	res.render('home');
