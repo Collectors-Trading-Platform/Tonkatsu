becb40cb0b64d85450d1a1f45f43ef41ecf672f8
tomki1 <38170380+tomki1@users.noreply.github.com>
2020-07-01 00:29:03 -0700

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -1,9 +1,1 @@
-var mysql = require('mysql');
-var db_config = {
-  	host            : 'us-cdbr-east-02.cleardb.com',
- 	user            : 'b5740ca304b5bc',
- 	password        : 'fee9d940',
- 	database        : 'heroku_f8290df26fb9a01'
-};
 
-var connection;
