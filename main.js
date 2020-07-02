9d50fda13b176ec661342ce163e120f2d7d6db77
tomki1 <38170380+tomki1@users.noreply.github.com>
2020-07-02 00:19:54 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,49 @@
+var express = require('express');
+var mysql = require('./dbcon.js');
+var bodyParser = require('body-parser');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/static', express.static('public'));
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 5000);
+app.set('mysql', mysql);
+app.use('/workers', require('./workers.js'));
+app.use('/products', require('./products.js'));
+app.use('/customers', require('./customers.js'));
+app.use('/locations', require('./locations.js'));
+app.use('/sections', require('./sections.js'));
+app.use('/customersproducts', require('./customersproducts.js'));
+app.use('/', express.static('public'));
+
+app.get('/:1',function(req,res) {
+	res.render('home');
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.status(err.status || 500);
+  res.json({
+  message: err.message,
+  error: err
+});
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
+
 /***
 var express = require('express');
 // var mysql = require('./dbcon.js');
