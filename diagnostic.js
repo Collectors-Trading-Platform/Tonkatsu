a80907353d50b4c53839fd415567dd70e6b94077
Jason Lim <31980053+jhhlim@users.noreply.github.com>
2020-07-01 20:24:37 -0700

    Update diagnostic.js

--- diagnostic.js
+++ diagnostic.js
@@ -7,18 +7,19 @@ var router = express.Router();
 
 
 var mysql = require('mysql');
-var db_config = {
+var pool = mysql.createPool( {
   	host            : 'us-cdbr-east-02.cleardb.com',
  	user            : 'b5740ca304b5bc',
  	password        : 'fee9d940',
  	database        : 'heroku_f8290df26fb9a01'
-};
+});
 
 var connection;
 
 function handleDisconnect() {
-    console.log('1. connecting to db:');
-    connection = mysql.createConnection(db_config); // Recreate the connection, since
+    console.log('1. connecting to 
+		:');
+    connection = mysql.createConnection(pool); // Recreate the connection, since
 													// the old one cannot be reused.
 
     connection.connect(function(err) {              	// The server is either down
@@ -55,14 +56,14 @@ module.exports = function(){
 
 app.get('/reset-table',function(req,res,next){
   var context = {};
-  mysql.connection.query("DROP TABLE IF EXISTS customerstable", function(err){
+  mysql.pool.query("DROP TABLE IF EXISTS customerstable", function(err){
     var createString = "CREATE TABLE customerstable("+
     "cid INT PRIMARY KEY AUTO_INCREMENT,"+
     "cFirstName VARCHAR(50) NOT NULL,"+
     "cLastName VARCHAR(50) NOT NULL,"+
     "gender VARCHAR(50) NOT NULL,"+
     "hometown VARCHAR(50) NOT NULL)";
-    mysql.connection.query(createString, function(err){
+    mysql.pool.query(createString, function(err){
       context.results = "Table reset";
       res.render('reset',context);
     })
@@ -72,7 +73,7 @@ app.get('/reset-table',function(req,res,next){
 
 function getCustomers(res, mysql, context, complete){
 	var context = {};
-	mysql.connection.query("SELECT cid, cFirstName, cLastName, gender, hometown from customerstable",
+	mysql.pool.query("SELECT cid, cFirstName, cLastName, gender, hometown from customerstable",
 	 function(err, results, fields){
 	    if(err){
 		res.write(JSON.stringify(err));
