6dca37a91fde5c5379468727fac9e6e2883b3eba
tomki1 <38170380+tomki1@users.noreply.github.com>
2020-07-01 23:00:13 -0700

    Update diagnostic.js

--- diagnostic.js
+++ diagnostic.js
@@ -7,29 +7,30 @@ const handlebars = require('express-handlebars');// .create({defaultLayout:'main
 // app.use(express.logger());
 
 var mysql = require('mysql');
-var db_config = {
+//var db_config = {
+var pool = mysql.createConnection({
   	host            : 'us-cdbr-east-02.cleardb.com',
  	user            : 'b5740ca304b5bc',
  	password        : 'fee9d940',
  	database        : 'heroku_f8290df26fb9a01'
-};
+});
 
-var connection;
+//var connection;
 
 
 function handleDisconnect() {
     console.log('1. connecting to db:');
-    connection = mysql.createConnection(db_config); // Recreate the connection, since
-													// the old one cannot be reused.
+    //connection = mysql.createConnection(db_config); // Recreate the connection, since
+    pool;													// the old one cannot be reused.
 
-    connection.connect(function(err) {              	// The server is either down
+    pool.connect(function(err) {              	// The server is either down
         if (err) {                                     // or restarting (takes a while sometimes).
             console.log('2. error when connecting to db:', err);
             setTimeout(handleDisconnect, 1000); // We introduce a delay before attempting to reconnect,
         }                                     	// to avoid a hot loop, and to allow our node script to
     });                                     	// process asynchronous requests in the meantime.
     											// If you're also serving http, display a 503 error.
-    connection.on('error', function(err) {
+    pool.on('error', function(err) {
         console.log('3. db error', err);
         if (err.code === 'PROTOCOL_CONNECTION_LOST') { 	// Connection to the MySQL server is usually
             handleDisconnect();                      	// lost due to either server restart, or a
