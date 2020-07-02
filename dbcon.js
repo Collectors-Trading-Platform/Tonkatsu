69691b6c2a45ec6bdd13d7feb53ec4a8e1f17acf
tomki1 <38170380+tomki1@users.noreply.github.com>
2020-07-02 00:17:09 -0700

    Create dbcon.js

---
+++ dbcon.js
@@ -0,0 +1,10 @@
+var mysql = require('mysql');
+var pool = mysql.createPool({
+	connectionLimit : 10,
+  	host            : 'us-cdbr-east-02.cleardb.com',
+ 	user            : 'b5740ca304b5bc',
+ 	password        : 'fee9d940',
+ 	database        : 'heroku_f8290df26fb9a01'
+});
+
+module.exports.pool = pool;
