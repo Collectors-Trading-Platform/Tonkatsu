eaeb85048cd9bd6c8d15ca8f4dc1c7dfefdebf48
jhhlim <31980053+jhhlim@users.noreply.github.com>
2021-04-15 23:07:00 -0700

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var db_config = {
-	host            : 'us-cdbr-east-02.cleardb.com',
-	user            : 'b5b7cb5d8126ac',
-	password        : 'b296bab6',
-	database        : 'heroku_fd3a3a2208266f1'
+	host            : 
+	user            : 
+	password        : 
+	database        : 
 };
 
 var connection;
